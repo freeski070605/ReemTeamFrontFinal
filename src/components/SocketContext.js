f8fe2db32ced9dcc8aaa5877bcb6fa09b77b713d
freeski070605 <65510732+freeski070605@users.noreply.github.com>
2025-06-26 05:30:03 -0400

    Update SocketContext.js

--- src/components/SocketContext.js
+++ src/components/SocketContext.js
@@ -5,7 +5,7 @@ import { io } from 'socket.io-client';
 export const SocketContext = createContext();
 
 // Create socket instance
-const socket = io('http://localhost:5000', {
+const socket = io('https://reem-team-front-final.vercel.app/', {
   transports: ['websocket', 'polling'],
   reconnection: true,
   reconnectionAttempts: 5,
