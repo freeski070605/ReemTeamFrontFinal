d8f85f6007ad3279a3aa872a6061abafff327f28
Drew <freeski0706@outlook.com>
2025-06-26 22:21:28 -0400

    sc again

--- src/components/SocketContext.js
+++ src/components/SocketContext.js
@@ -1,4 +1,4 @@
-import React, { createContext, useState, useEffect } from 'react';
+import React, { createContext, useState, useEffect, useCallback } from 'react';
 import { io } from 'socket.io-client';
 
 export const SocketContext = createContext();
